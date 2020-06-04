52c1663ef58a30de49645aa95da6d7ec4ba50caa
Drokight <42219144+Drokight@users.noreply.github.com>
2020-06-04 18:23:51 -0400

    Update Ejercicio 6.c

--- Ejercicios C/Ejercicio 6.c
+++ Ejercicios C/Ejercicio 6.c
@@ -1,3 +1,6 @@
+//Pasar de segundos a días, horas y minutos. Escribe un programa que pida al usuario 
+//que introduzca los segundos, y le conteste diciéndole el número de días, horas, minutos y segundos que son. 
+
 #include<stdio.h>
 void main(){
 	int segundos, minutos, horas, dias;
