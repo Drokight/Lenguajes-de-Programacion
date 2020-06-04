3e0498ae1751d686f99bffde6ebeac892aea804f
Drokight <42219144+Drokight@users.noreply.github.com>
2020-06-04 18:26:53 -0400

    Update Ejercicio 8.c

--- Ejercicios C/Ejercicio 8.c
+++ Ejercicios C/Ejercicio 8.c
@@ -1,3 +1,6 @@
+//Cálculo de la resistencia equivalente. Escribir un programa que pida por teclado 
+//dos resistencias y calcule y presente la resistencia equivalente en paralelo (Req=(R1*R2)/(R1+R2)). 
+
 #include<stdio.h>
 void main(){
 	double resistenciaE, resistencia1, resistencia2;
