2abd7d4acedee906ea875a1dfa8c435c7c532173
Drokight <42219144+Drokight@users.noreply.github.com>
2020-06-04 18:19:08 -0400

    Add files via upload

---
+++ Ejercicio 9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+	double resultado;
+	int a,b,c,d;
+	
+	printf("Ingrese el valor 1: ");
+	scanf("%d",&a);
+	printf("Ingrese el valor 2: ");
+	scanf("%d",&b);
+	printf("Ingrese el valor 3: ");
+	scanf("%d",&c);
+	printf("Ingrese el valor 4: ");
+	scanf("%d",&d);
+	
+	resultado= a+b+c+d;
+	resultado/=4;
+	printf("La media de los cuatro valores es %lf",resultado);
+}
