1e09b315a9a68dd58b67468575245b15a3bceb99
Drokight <42219144+Drokight@users.noreply.github.com>
2020-06-04 18:22:50 -0400

    Update Ejercicio 4.c

--- Ejercicios C/Ejercicio 4.c
+++ Ejercicios C/Ejercicio 4.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
 //Cálculo de perímetro de circunferencia, área del círculo, y volumen de la esfera. 
 //Suponiendo que pi = 3.1416. 
 //Escribe un programa que pida al usuario que introduzca el radio, y 
 //presente por pantalla el cálculo del perímetro de la circunferencia (2*pi*r), 
 //el área del círculo (pi*r2 ), y el volumen de la esfera (V = 4*pi*r3 /3). 
+
+#include<stdio.h>
 
 void main(){
 	int radio;
