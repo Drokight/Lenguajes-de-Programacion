8ad9d5f1cf7c89a78896420a0f27787949b266a6
Drokight <42219144+Drokight@users.noreply.github.com>
2020-06-04 18:26:24 -0400

    Update and rename Ejercicio 9.c to Ejercicios C/Ejercicio 9.c

--- Ejercicios C/Ejercicio 9.c
+++ Ejercicios C/Ejercicio 9.c
@@ -1,3 +1,5 @@
+//Media de cuatro números. Escribir un programa que pida por teclado cuatro números y calcule y presente la media de los cuatro.
+
 #include<stdio.h>
 void main(){
 	double resultado;
