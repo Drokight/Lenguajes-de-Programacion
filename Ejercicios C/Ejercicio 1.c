8f6666fdacbacc9c9ee95b782e1c4838ea4bb3eb
Drokight <42219144+Drokight@users.noreply.github.com>
2020-06-04 18:21:10 -0400

    Update Ejercicio 1.c

--- Ejercicios C/Ejercicio 1.c
+++ Ejercicios C/Ejercicio 1.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+//Cálculo de precios con descuento 
+//Escribe un programa que pregunte el precio, el tanto por ciento de descuento, y te diga el precio con descuento. 
+//Por ejemplo, si el precio que introduce el usuario es 300 y el descuento 20, 
+//el programa dirá que el precio final con descuento es de 240. 
+
 void main(){
 	int precio;
 	int descuento; 
